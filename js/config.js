fab0f6e2b36f32ee57ec7e55d3f9b25879ff2cbb
generativeai-htn <generativeai@htn.ac.th>
2026-07-21 09:57:29 +0700

    Configure student records endpoint

--- js/config.js
+++ js/config.js
@@ -2,5 +2,5 @@
  * Paste the Google Apps Script Web App /exec URL below after deployment.
  */
 window.PC8_CONFIG = Object.freeze({
-  studentDataEndpoint: ""
+  studentDataEndpoint: "https://script.google.com/macros/s/AKfycbzLE28HBr8HbbtzkpPWLcw3XJjYWyqpJZkOOo9NlpIxBieRPFg2dCKeK6m_g4Y7cgbM/exec"
 });
